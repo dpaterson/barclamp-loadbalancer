8309cd1e9ba2e4ec488f25f56faee0818755a129
Maxim Datskovskiy <axamus@gmail.com>
2013-03-28 15:19:23 +0200

    update

---
+++ chef/roles/loadbalancer.rb
@@ -0,0 +1,11 @@
+name "loadbalancer"
+description "Floating IP active/passive IP service HA provider"
+run_list(
+   "recipe[chef-keepalived::default]"
+   "recipe[chef-keepalived::ip_nonlocal_bind]"
+   "recipe[pound::default]"
+)
+default_attributes(
+)
+override_attributes()
+
