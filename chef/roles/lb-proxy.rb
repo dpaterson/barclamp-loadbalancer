39423e477213ae11d5449390bb7b0493aa056a2c
David Paterson <david_paterson@dell.com>
2013-05-04 01:22:28 -0400

    Adding changes from pull request for testing

--- chef/roles/lb-proxy.rb
+++ chef/roles/lb-proxy.rb
@@ -1,8 +1,7 @@
-
 name "keepalived"
 description "Floating IP active/passive IP service HA provider"
 run_list(
-   "recipe[chef-keepalived::default]",
+   "recipe[chef-keepalived::default]"
    "recipe[chef-keepalived::ip_nonlocal_bind]"  
 )
 default_attributes(
